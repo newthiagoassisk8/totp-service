2bb6dc21e538aedcb44e9c82e9d7540f0faf4be6
Thiago Assis <assisthiago2303@gmail.com>
2025-12-30 13:54:25 -0300

    feat: add vite-tsconfig-paths for improved path resolution in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    plugins: [tsconfigPaths()],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/server.ts'),
